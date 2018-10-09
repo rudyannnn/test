23e2a67fb1911b8fa2a2ec83a52dee807ce072c6
rudyannnn <43912095+rudyannnn@users.noreply.github.com>
2018-10-09 13:44:28 +0900

    Update Link.sh

--- Link.sh
+++ Link.sh
@@ -1,6 +1,7 @@
 JR="ssh -p 5xxxx o*@125.186.227.47" 
 BD="ssh -p 5xxxx o*@122.46.185.94" 
 SR="ssh -p 5xxxx o*@125.186.227.47" 
+SRI="ssh osmc@192.168.219.102"
 
 if [ $1="BD" ] 
     then
@@ -11,4 +12,7 @@ elif [ $1="JR" ]
 elif [ $1="SR" ]
     then
         eval $SR 
+elif [ $1="SRI" ]
+    then
+        eval $SRI 
 fi   
