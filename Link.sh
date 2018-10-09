db5105deccfb008c8f3eb743ec89a2865731c015
rudyannnn <43912095+rudyannnn@users.noreply.github.com>
2018-10-09 13:36:56 +0900

    외부 접속용 기록입니다.

---
+++ Link.sh
@@ -0,0 +1,14 @@
+JR="ssh -p 5xxxx o*@125.186.227.47" 
+BD="ssh -p 5xxxx o*@122.46.185.94" 
+SR="ssh -p 5xxxx o*@125.186.227.47" 
+
+if [ $1="BD" ] 
+    then
+        eval $BD
+elif [ $1="JR" ]
+    then
+        eval $JR
+elif [ $1="SR" ]
+    then
+        eval $SR 
+fi   
